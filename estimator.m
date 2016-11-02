43c34ef4d1f94ed85087a4654cd1292aaa8f2105
Brian Searls <briansrls@gmail.com>
2016-11-02 12:19:06 -0400

    Update estimator.m
    Changed estimate

--- estimator.m
+++ estimator.m
@@ -3,7 +3,7 @@ imagefiles = dir('test/*.png');
 for i = 1:size(imagefiles, 1)
     a = imread(strcat('test/', imagefiles(i).name));
     b = zeros(size(a));
-    b(:, :, :) = a(:, :, :) & 255;
+    b(:, :, :) = a(:, :, :) > 250;
     c = zeros(size(b, 1), size(b , 2));
     c = b(:, :, 1) & b(:, :, 2) & b(:, :, 3);
     ratio = sum(sum(c ~= 1)) / sum(sum(c ~= 0));
