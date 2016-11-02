4b96b4a58735c706c64cbaf8b2d959612b761ded
Brian Searls <briansrls@gmail.com>
2016-11-01 22:10:13 -0400

    further optimizations

--- estimator.m
+++ estimator.m
@@ -2,12 +2,10 @@ imagefiles = dir('test/*.png');
 
 for i = 1:size(imagefiles, 1)
     a = imread(strcat('test/', imagefiles(i).name));
-    imagefiles(i).name
     b = zeros(size(a));
-    for i = 1:size(a, 3)
-        b(:, :, i) = a(:, :, i) & 255;
-    end
+    b(:, :, :) = a(:, :, :) & 255;
     c = zeros(size(b, 1), size(b , 2));
     c = b(:, :, 1) & b(:, :, 2) & b(:, :, 3);
-    ratio = sum(sum(c ~= 1)) / numel(c)
+    ratio = sum(sum(c ~= 1)) / sum(sum(c ~= 0));
+    fprintf(strcat(imagefiles(i).name,  ': %f\n'), ratio);
 end
